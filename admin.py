d0a217b3bfaa4bf69acb79897c2d0913cfbf199b
johmstone <61640703+johmstone@users.noreply.github.com>
2021-04-06 23:32:26 -0600

    Update

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-from requests import Request, Session
+import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
@@ -11,12 +11,18 @@ class Criptomoneda(object):
         self.nombre = nombre
         self.saldo = saldo
         self.cotizacion = cotizacion
-    
-    def calcularSaldo(self, moneda):  
-        if moneda == "USD":
-            return self.saldo*self.cotizacion
-        else:
-            return self.saldo
+
+    def indicarRate(self, cotizacion):
+        self.cotizacion = cotizacion
+
+    def DepositoDirecto(self, monto):
+        self.saldo = self.saldo + monto
+
+    def DepositoenUSD(self, monto):
+        self.saldo = self.saldo + (monto/self.cotizacion)
+
+    def calcularSaldo(self):  
+        return self.saldo*self.cotizacion        
             
     def serialize(self):
         return {
@@ -27,31 +33,32 @@ class Criptomoneda(object):
         }
 
 def getMoneyDetail(id):
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest' + str(id)
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=' + str(id)
+    # print(url)
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': 'd23ea42b-4f22-47b3-8e28-4650f23c4096',
     }
 
-    session = Session()
-    session.headers.update(headers)
-
+    # session = Session()
+    # session.headers.update(headers)
     try:
-        response = session.get(url)
+        response =  requests.get(url, headers=headers)
         data = json.loads(response.text)
-        print(data)
+        return(data)
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
 
-def initialdata():
-    
+def initialdata():    
     monedero.append(Criptomoneda(1,1,'Bitcoins',0,0))
     monedero.append(Criptomoneda(1027,2,'Ethereum',0,0))
     monedero.append(Criptomoneda(1839,3,'Binance Coin',0,0))
     monedero.append(Criptomoneda(52,4,'XRP',0,0))
 
-    for item in monedero
-
+    for item in monedero:
+        detail = getMoneyDetail(item.id)
+        item.indicarRate(detail['data']["%s" % (item.id)]['quote']['USD']['price']) 
+        # print(detail['data']["%s" % (item.id)]['quote']['USD']['price']) 
 
 def getmonedas():
     print("Tipo de Moneda")
@@ -83,6 +90,8 @@ menu()
 option = int(input("Seleccione una opcion:"))
 
 if option == 1:
+    print("===================================================================")
+    print("====================== RECIBIR DINERO =============================")
     getmonedas()
     
     moneyType = int(input("Seleccione la moneda?"))
@@ -90,13 +99,14 @@ if option == 1:
 
     for item in monedero:
         if item.internalid == moneyType:
-            item.saldo = item.saldo + moneyQty
+            item.DepositoDirecto(moneyQty)
 
     # do stuff
+    print("===================================================================")
+    print("============================= HECHO ===============================")
     for item in monedero:
-        print(item.id, item.nombre,item.saldo)
-
-
+        print("=== Nuevo balance:",item.saldo, item.nombre, "==> Saldo en USD:", item.calcularSaldo(),"=====================")
+    print("===================================================================")
 
 elif option == 2:
     print("option 2")
@@ -106,3 +116,6 @@ elif option == 4:
     print("option 4")
 else:
     print("option 5")
+
+menu()
+option = int(input("Seleccione una opcion:"))
